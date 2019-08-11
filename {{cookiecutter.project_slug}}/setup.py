1d0e5f9b6b60c2009d1214fd42a2f56d9a5d4f9a
Kazuya Takei <attakei@gmail.com>
2019-08-12 01:05:07 +0900

    Move options section from setup.py to setup.cfg

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -6,26 +6,6 @@
 from setuptools import setup, find_packages
 
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=6.0',{%- endif %} ]
-
-setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %} ]
-
-test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest',{%- endif %} ]
-
-
 setup(
-    {%- if 'no' not in cookiecutter.command_line_interface|lower %}
-    entry_points={
-        'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
-        ],
-    },
-    {%- endif %}
-    install_requires=requirements,
-    include_package_data=True,
-    packages=find_packages(include=['{{ cookiecutter.project_slug }}']),
-    setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
-    zip_safe=False,
 )
